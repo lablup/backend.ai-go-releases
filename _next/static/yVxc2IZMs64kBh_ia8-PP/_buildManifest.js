00a68dd87336fb026244bef64bb305ab28b291cc
Jeongkyu Shin <inureyes@gmail.com>
2026-01-05 08:36:04 -0800

    Deploy: 2026-01-05 08:36:04

---
+++ _next/static/yVxc2IZMs64kBh_ia8-PP/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/backend.ai-go-releases//_next/:path+",
+        "destination": "/backend.ai-go-releases/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
